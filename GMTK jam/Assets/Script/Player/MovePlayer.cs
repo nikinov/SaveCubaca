a419b87b6c4780d5573f4f79fdbe796c08096654
nikinov <novellenicholas@gmail.com>
2020-07-11 10:59:36 +0200

    enemy prototype

---
+++ GMTK jam/Assets/Script/Player/MovePlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += move * Time.deltaTime * moveSpeed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("dd");
+        if (collision.gameObject.tag == "Enemy")
+        {
+            LeanTween.scale(gameObject, new Vector3(0f, 0f), .3f).setEase(LeanTweenType.easeOutBounce);
+            StartCoroutine(wait(gameObject));
+        }
+    }
+    IEnumerator wait(GameObject g)
+    {
+        yield return new WaitForSeconds(.3f);
+        Destroy(g);
+    }
+}
---
+++ GMTK jam/Assets/Script/other/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Player;
+    void Start()
+    {
+        LeanTween.scale(Player, new Vector3(0.135f, 0.135f), 1.35f).setEase(LeanTweenType.easeOutBounce);
+    }
+}

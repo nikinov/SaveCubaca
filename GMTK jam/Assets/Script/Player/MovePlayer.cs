3de20c6a7e86dbc1c8983a58b0a970d7cc791fdb
nikinov <novellenicholas@gmail.com>
2020-07-11 14:29:33 +0200

    mainMenu

--- GMTK jam/Assets/Script/Player/MovePlayer.cs
+++ GMTK jam/Assets/Script/Player/MovePlayer.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MovePlayer : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
-
+    public GameManager gameManager;
+    private void Start()
+    {
+        LeanTween.scale(gameObject, new Vector3(0.17f, 0.17f), 1f).setEase(LeanTweenType.easeOutBounce);
+        LeanTween.rotateAround(gameObject, Vector3.forward, -720f, 1f);
+        LeanTween.move(gameObject, gameObject.transform.position + new Vector3(3.5f, -1f, 0f), 1f);
+        gameManager.BfadeOut(.7f);
+    }
     // Update is called once per frame
     void Update()
     {
@@ -14,16 +22,35 @@ public class MovePlayer : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("dd");
         if (collision.gameObject.tag == "Enemy")
         {
             LeanTween.scale(gameObject, new Vector3(0f, 0f), .3f).setEase(LeanTweenType.easeOutBounce);
             StartCoroutine(wait(gameObject));
         }
+        else if (collision.gameObject.tag == "Start")
+        {
+            gameManager.BfadeIn(.4f);
+            LeanTween.scale(gameObject, new Vector3(0f, 0f), .6f).setEase(LeanTweenType.easeOutBounce);
+            LeanTween.rotateAround(gameObject, Vector3.forward, -360f, .6f);
+            LeanTween.move(gameObject, gameObject.transform.position + new Vector3(2f, 1f, 0f), .6f);
+            StartCoroutine(wait(gameObject, true, 0));
+        }
+        else if (collision.gameObject.tag == "Base")
+        {
+            gameManager.BfadeIn(.4f);
+            LeanTween.scale(gameObject, new Vector3(0f, 0f), .6f).setEase(LeanTweenType.easeOutBounce);
+            LeanTween.rotateAround(gameObject, Vector3.forward, 360f, .6f);
+            LeanTween.move(gameObject, gameObject.transform.position + new Vector3(-2f, 1f, 0f), .6f);
+            StartCoroutine(wait(gameObject, true, 1));
+        }
     }
-    IEnumerator wait(GameObject g)
+    IEnumerator wait(GameObject g, bool Stay = true, int scene = 2)
     {
-        yield return new WaitForSeconds(.3f);
+        yield return new WaitForSeconds(.6f);
         Destroy(g);
+        if (!Stay)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + scene);
+        }
     }
 }

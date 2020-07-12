f68e93a6ce0adcebc39d8dcfa98b8f08ab60c3c4
nikinov <novellenicholas@gmail.com>
2020-07-12 13:19:26 +0200

    ...

---
+++ backup/Assets/Script/Player/camZoom.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camZoom : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] private float speed;
+    private int zoom;
+    [SerializeField] private float corner0;
+    [SerializeField] private float corner1;
+    private void Start()
+    {
+        cam = Camera.main;
+        zoom = 1;
+    }
+    private void Update()
+    {
+        if (zoom == 1)
+        {
+            if (cam.orthographicSize > 2)
+            {
+                cam.orthographicSize = cam.orthographicSize - 1 * Time.deltaTime * speed;
+                this.transform.position += new Vector3(Time.deltaTime * speed * 1.8f * corner0, Time.deltaTime * speed * corner1);
+            }
+            else
+            {
+                cam.orthographicSize = 2;
+                zoom = 0;
+                StartCoroutine(wait(.5f));
+            }
+        }
+        else if (zoom == 2)
+        {
+            if (cam.orthographicSize < 5)
+            {
+                cam.orthographicSize = cam.orthographicSize + 1 * Time.deltaTime * speed;
+                this.transform.position += new Vector3(Time.deltaTime * speed * 1.8f * -corner0, Time.deltaTime * speed * -corner1);
+            }
+            else
+            {
+                this.transform.position = new Vector3(0, 0, -10);
+                cam.orthographicSize = 5;
+                zoom = 0;
+            }
+        }
+    }
+    IEnumerator wait(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+        zoom = 2;
+    }
+}
---
+++ backup/Assets/Script/other/changee.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changee : MonoBehaviour
+{
+    public GameObject portal;
+    public GameObject newSpike;
+    private void Start()
+    {
+        newSpike.SetActive(false);
+        newSpike.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        newSpike.SetActive(true);
+        LeanTween.scale(portal, new Vector3(0,0,0), 1);
+        newSpike.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        newSpike.GetComponent<Rigidbody2D>().AddForce(new Vector3(-15,0,0));
+        Destroy(gameObject);
+        
+    }
+}
